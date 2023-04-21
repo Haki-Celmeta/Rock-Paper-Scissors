8fd86604aa0f404f010e1b932f7bccc58de9341e
Haki Celmeta <celmeta.haki@gmail.com>
2023-04-21 20:05:26 +0200

    Finished User Interface of the all the game

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const gameMoves = ["rock", "paper", "scissors"];
+const rock = document.querySelector("#rock");
+const papper = document.querySelector("#papper");
+const scissors = document.querySelector("#scissors");
+let playerPoints = document.querySelector("#player-point");
+let computerPoints = document.querySelector("#computer-point");
+let won = document.querySelector("#won");
+let loose = document.querySelector("#loose");
+let wonLooseContainer = document.querySelector(".won-loose-container");
+let whoWon = document.querySelector(".who-won");
+let repeatButton = document.querySelector("#repeat-button");
+let totalPlayerGame = document.querySelector("#total-player-game");
+let totalComputerGame = document.querySelector("#total-computer-game");
 let playerWins = 0;
 let computerWins = 0;
+let totalGamePl = 0;
+let totalGameCp = 0;
+const gameMoves = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
   return gameMoves[Math.floor(Math.random() * gameMoves.length)];
@@ -9,44 +23,85 @@ function getComputerChoice() {
 function selection(playerSelection, computerSelection) {
   let ps = playerSelection.toLowerCase();
   if (ps === computerSelection) {
-    return "draw, play again, adjfaosdijfvjdf;vj";
+    return 0;
   } else if (ps === "rock" && computerSelection === "paper") {
-    computerWins++;
-    return "You lose! Paper beats rock";
+    return -1;
   } else if (ps === "rock" && computerSelection === "scissors") {
-    playerWins++;
-    return "You win! Rock beats scissors";
+    return 1;
   } else if (ps === "paper" && computerSelection === "rock") {
-    playerWins++;
-    return "You win! Paper beats rock";
+    return 1;
   } else if (ps === "paper" && computerSelection === "scissors") {
-    computerWins++;
-    return "You lose! Scissors beats paper";
+    return -1;
   } else if (ps === "scissors" && computerSelection === "rock") {
-    computerWins++;
-    return "You lose! Rock beats scissors";
+    return -1;
   } else if (ps === "scissors" && computerSelection === "paper") {
-    playerWins++;
-    return "You win! Scissors beats paper";
+    return 1;
   } else {
-    return "Error 101";
+    return 2;
   }
 }
 
-for (let i = 0; ; i++) {
+let isClicked = true;
+let ps = "";
+rock.addEventListener("click", function () {
+  if (isClicked) {
+    ps = "rock";
+    playGame();
+  }
+});
+papper.addEventListener("click", function () {
+  if (isClicked) {
+    ps = "paper";
+    playGame();
+  }
+});
+scissors.addEventListener("click", function () {
+  if (isClicked) {
+    ps = "scissors";
+    playGame();
+  }
+});
+repeatButton.addEventListener("click", function () {
+  playerWins = 0;
+  computerWins = 0;
+  isClicked = true;
+  playerPoints.innerText = `You: 0 points`;
+  computerPoints.innerText = `Computer: 0 points`;
+  whoWon.style.visibility = "hidden";
+  wonLooseContainer.style.display = "none";
+});
+
+function playGame() {
+  let decision = selection(ps, getComputerChoice());
+
+  if (decision === 1) {
+    playerWins++;
+    whoWon.innerText = "You win this round";
+    whoWon.style.visibility = "visible";
+  } else if (decision === -1) {
+    computerWins++;
+    whoWon.innerText = "Computer win this round";
+    whoWon.style.visibility = "visible";
+  } else {
+    whoWon.innerText = "Draw";
+    whoWon.style.visibility = "visible";
+  }
+  playerPoints.innerText = `You: ${playerWins} points`;
+  computerPoints.innerText = `Computer: ${computerWins} points`;
+
   if (playerWins === 5) {
-    console.log("Player Wins the game ===================");
-    break;
+    totalGamePl++;
+    totalPlayerGame.innerText = `You: ${totalGamePl}`;
+    won.innerText = `You Won`;
+    loose.innerText = `Computer Loose`;
+    wonLooseContainer.style.display = "flex";
+    isClicked = false;
   } else if (computerWins === 5) {
-    console.log("Computer Wins the game =======================");
-    break;
+    totalGameCp++;
+    totalComputerGame.innerText = `Computer: ${totalGameCp}`;
+    won.innerText = `Computer Won`;
+    loose.innerText = `You Loose`;
+    wonLooseContainer.style.display = "flex";
+    isClicked = false;
   }
-
-  let playerSelection = String(prompt("Write (Rock, Paper or Scissors): "));
-  let computerSelection = getComputerChoice();
-
-  console.log(computerSelection);
-  console.log(selection(playerSelection, computerSelection));
 }
-
-console.log("Game finished");
